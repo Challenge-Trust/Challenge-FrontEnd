627934e3d4d7bdea69acbeb052dd643032e15c6b
frazaomol <nicolasfv.molina@gmail.com>
2026-09-12 18:24:36 -0300

    arrumei os erros em textos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,7 +25,7 @@ const features = [
     text: 'Gere vouchers com QR Code para uso imediato nas catracas e terminais parceiros.',
   },
   {
-    title: 'Integração com bilhetagem',
+    title: 'Integração com Bilhetagem',
     text: 'Crédito automático no seu cartão de transporte.',
   },
   {
